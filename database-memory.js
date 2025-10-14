49609744f50e9adceba08484a82b402e0406d66b
LuizBlume <luiz.fernando.251107@gmail.com>
2025-10-14 16:55:35 -0300

    Criação do node com Express

---
+++ database-memory.js
@@ -0,0 +1,18 @@
+import { randomUUID } from "node:crypto"
+
+export class DatabaseMemory {
+    #videos = new Map()
+    list() {
+        return this.#videos.values()
+    }
+    create(video) {
+        const videoId = randomUUID()
+        this.#videos.set(videoId, video)
+    }
+    update(id, video) {
+        this.#videos.push(id, video)
+    }
+    delete(id) {
+        this.#videos.delete(id)
+    }
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
+const server = fastify();
+const database = new DatabaseMemory()
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body
+    database.create({
+        title,
+        description,
+        duration
+    })
+    return reply.status(201).send()
+})
+server.get('/videos', () => {
+    return 'Hello World!'
+})
+server.put('/videos/:id', () => {
+    return 'Hello World!'
+})
+server.delete('/videos/:id', () => {
+    return 'Hello World!'
+})
+server.listen({
+    port: 3333,
+});
